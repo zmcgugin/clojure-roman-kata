bfc7d084979efe16076c0cf8ff64c105aaff9087
Zak <zmcgugin@gmail.com>
2017-08-26 18:31:15 -0400

    adding V test

--- src/clojure_roman_kata/core.clj
+++ src/clojure_roman_kata/core.clj
@@ -1,11 +1,33 @@
 (ns clojure-roman-kata.core)
 
-  (defn convert [x y]
-    (apply str (repeat x y)))
+(def symbolMap (sorted-map
+  5 "V"
+  1 "I"
+))
 
-  (defn -main "Main function thingy" [x]
-    (convert x "I"))
+(defn getKeyForBiggestMatch [value]
+  (first (filter #(<= % @value) (reverse(keys symbolMap))))
+)
 
+(defn searchForHighestMatch [remaining]
+  (def biggest (getKeyForBiggestMatch remaining))
+  (reset! remaining (- @remaining biggest))
+  (get symbolMap biggest)
+)
+
+(defn convert [value]
+  (def finalString (atom ""))
+  (def remaining (atom value))
+  (while (> @remaining 0)
+    (def partialS (apply str (searchForHighestMatch remaining)))
+    (reset! finalString (apply str @finalString partialS)))
+  @finalString
+)
+
+
+(defn -main "Main function thingy" [value]
+  (convert value)
+)
 
 
 
--- test/clojure_roman_kata/core_test.clj
+++ test/clojure_roman_kata/core_test.clj
@@ -6,8 +6,10 @@
   (testing "Givin 1 return I"
     (is (= (-main 1) "I"))))
 
-
 (deftest a-test2
   (testing "Givin 2 return II"
     (is (= (-main 2) "II"))))
 
+(deftest a-test3
+  (testing "Givin 5 return V"
+    (is (= (-main 5) "V"))))
