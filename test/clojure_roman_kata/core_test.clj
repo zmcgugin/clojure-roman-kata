a7e4ccd5d5c90f0dcca1ea80588181c97827a7de
Zak <zmcgugin@gmail.com>
2017-08-26 22:45:57 -0400

    adding IV test

--- test/clojure_roman_kata/core_test.clj
+++ test/clojure_roman_kata/core_test.clj
@@ -3,17 +3,21 @@
             [clojure-roman-kata.core :refer :all]))
 
 (deftest a-test
-  (testing "Givin 1 return I"
+  (testing "Given 1 return I"
     (is (= (-main 1) "I"))))
 
 (deftest a-test2
-  (testing "Givin 2 return II"
+  (testing "Given 2 return II"
     (is (= (-main 2) "II"))))
 
 (deftest a-test3
-  (testing "Givin 5 return V"
+  (testing "Given 5 return V"
     (is (= (-main 5) "V"))))
 
 (deftest a-test4
-  (testing "Givin 5 return V"
+  (testing "Given 6 return V"
     (is (= (-main 6) "VI"))))
+
+(deftest a-test5
+  (testing "Given 4 return IV"
+    (is (= (-main 4) "IV"))))
